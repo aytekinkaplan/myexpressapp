43a58812680c6b42bbd89b9ae9e935f6f46a4904
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-06-05 08:24:46 +0300

    New commit

--- project002/index.js
+++ project002/index.js
@@ -1,43 +1,43 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-app.get('/about',(req,res)=>{
+
+app.get('/about', (req, res) => {
     res.send("About Page");
 });
-app.get('/contact',(req,res)=>{
+
+app.get('/contact', (req, res) => {
     res.send("Contact Page");
 });
-app.get('/help',(req,res)=>{
+
+app.get('/help', (req, res) => {
     res.send("Help Page");
 });
-app.post('/signup',(req,res)=>{
+
+app.post('/signup', (req, res) => {
     res.send("Signup Page");
 });
-app.post('/login',(req,res)=>{
+
+app.post('/login', (req, res) => {
     res.send("Login Page");
 });
-app.get('/',(req,res)=>{
+
+app.get('/', (req, res) => {
     res.send("Home Page");
 });
-app.get('/help/*',(req,res)=>{
+
+app.get('/help/*', (req, res) => {
     res.send("Help Article Not Found");
 });
-app.get('/about/*',(req,res)=>{
+
+app.get('/about/*', (req, res) => {
     res.send("About Article Not Found");
 });
-app.get('/*',(req,res)=>{
+
+app.get('/*', (req, res) => {
     res.send("Page Not Found");
 });
-app.listen(port,()=>{
+
+app.listen(port, () => {
     console.log(`Example app listening on port: http://localhost:${port}`);
 });
-app.get('/help/*',(req,res)=>{
-    res.send("Help Article Not Found");
-});
-app.get('/about/*',(req,res)=>{
-    res.send("About Article Not Found");
-});
-app.get('/*',(req,res)=>{
-    res.send("Page Not Found");
-})
-
