675cd647178e1a600d415062d50d2a727cd9c5d8
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-06-05 23:57:09 +0300

    New commit

--- project005/index.js
+++ project005/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-app.use(express.static("public"));
-app.get('/', (req, res) => {
-    res.send("Home Page");
+const users = require('./users.json');
+app.get("/users", (req, res) => {
+    res.json(users);
 });
-
 app.listen(port, () => {
     console.log(`Example app listening on port: http://localhost:${port}`);
 });
+
